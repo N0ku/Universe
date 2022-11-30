e8391a7a21b7fd33eb58238b12d1cae8029aa6bc
N0ku <lucielecouturier2@gmail.com>
2022-11-30 22:30:14 +0100

    add tests textures for earth, update css and html

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import "./style.css";
 
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
 const scene = new THREE.Scene();
 
@@ -19,11 +20,9 @@ renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 camera.position.setZ(30);
 
-const geometry = new THREE.SphereGeometry(15, 32, 16);
+const geometry = new THREE.SphereGeometry(16, 32, 16);
 
-const texture = new THREE.TextureLoader().load(
-  "textures/8081_earthlights10k.jpg"
-);
+const texture = new THREE.TextureLoader().load("textures/earth.jpg");
 
 const material = new THREE.MeshBasicMaterial({
   map: texture,
@@ -31,12 +30,42 @@ const material = new THREE.MeshBasicMaterial({
 
 const earth = new THREE.Mesh(geometry, material);
 
+const sunLight = new THREE.AmbientLight(0x404040, 1);
+
+const canvas = document.getElementById("app");
+
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var targetMesh;
+
+function onMouseClick(event) {
+  raycaster.setFromCamera(mouse, camera);
+  var isIntersected = raycaster.intersectObject(targetMesh);
+  if (isIntersected) {
+    console.log("Mesh clicked!");
+  }
+}
+
+function onMouseMove(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+}
+canvas.addEventListener("mouseclick", onMouseClick);
+canvas.addEventListener("mousemove", onMouseMove);
+
+scene.add(sunLight);
+sunLight.position.set(0, 30, 0);
+
 scene.add(earth);
+
+const controls = new OrbitControls(camera, renderer.domElement);
 
 function animate() {
   requestAnimationFrame(animate);
 
-  earth.rotation.y += 0.005;
+  earth.rotation.y += 0.002;
+
+  controls.update();
   renderer.render(scene, camera);
 }
 
