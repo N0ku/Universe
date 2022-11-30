5b0a31d022e7cdb90e9a962c3fc545f8b213bf4f
N0ku <lucielecouturier2@gmail.com>
2022-11-30 10:41:35 +0100

    push project

---
+++ main.js
@@ -0,0 +1,42 @@
+import "./style.css";
+
+import * as THREE from "three";
+
+const scene = new THREE.Scene();
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#back"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+const geometry = new THREE.TorusGeometry(10, 3, 16, 100);
+
+const material = new THREE.MeshBasicMaterial({
+  color: 0x326ba8,
+  wireframe: true,
+});
+
+const torus = new THREE.Mesh(geometry, material);
+
+scene.add(torus);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
+  renderer.render(scene, camera);
+}
+
+animate();
